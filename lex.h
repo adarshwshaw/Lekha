3cc6d4aa7ffcde36099fb8d4d75c089e6ba204ba
Adarsh Shaw <adarshwshaw@gmail.com>
2026-05-17 18:09:58 +0530

    add new updates

--- lex.h
+++ lex.h
@@ -15,12 +15,33 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
+ *
+LexRule r[]={
+    [0]=symrule,
+    [1]=lr_whitespace
+};
+ *  version : 0.1.1
+ *  CHANGELOGS:
+ *  0.1.1
+ *    2026-05-10:
+ *    	- ADDED LEKHADEF to add static inline if needed to  remove unused function warning
+ *  0.1.2
+ *    2026-05-12:
+ *    	- fixed rule for skipping whitespace
+ *    	- fixed rule skip extra iteration after finding the matching rule
+ *    	- added dprintf for debug printing
+ *    2026-05-14:
+ *    	- fixed reallocation error while appending tokens
+ *  
  */
 
 #ifndef LEKHA_H
 #define LEKHA_H
 #include <stdint.h>
 
+#ifndef LEKHADEF
+    #define LEKHADEF
+#endif
 typedef struct {
     char* lexme;
     uint32_t size;
@@ -47,20 +68,20 @@ typedef struct {
     LexRule* list;
 } LexRules;
 
-static int lex_init(Lexer* lex, char* buff, uint32_t len);
+LEKHADEF int lex_init(Lexer* lex, char* buff, uint32_t len);
 
-static int lex_analyse(Lexer* lex, LexRules rules);
+LEKHADEF int lex_analyse(Lexer* lex, LexRules rules);
 
-static int lex_appendLToken(LTokens* toks, LToken tok);
+LEKHADEF int lex_appendLToken(LTokens* toks, LToken tok);
 
-static int lex_stringstart(const char* buf);
-static int lex_stringhandle(Lexer* lex, uint32_t i);
+LEKHADEF int lex_stringstart(const char* buf);
+LEKHADEF int lex_stringhandle(Lexer* lex, uint32_t i);
 
-static int lex_numberstart(const char* buf);
-static int lex_numberhandle(Lexer* lex, uint32_t i);
+LEKHADEF int lex_numberstart(const char* buf);
+LEKHADEF int lex_numberhandle(Lexer* lex, uint32_t i);
 
-static int lex_whitespacestart(const char* buf);
-static int lex_whitespacehandle(Lexer* lex, uint32_t i);
+LEKHADEF int lex_whitespacestart(const char* buf);
+LEKHADEF int lex_whitespacehandle(Lexer* lex, uint32_t i);
 
 
 typedef enum {
@@ -71,21 +92,9 @@ typedef enum {
     LE_TOK_LIST_ERROR
 } LexError;
 
-static LexRule lr_whitespace = {
-    .startsWith = lex_whitespacestart,
-    .handleLToken = lex_whitespacehandle
-};
-
-static LexRule lr_numbers = {
-    .startsWith = lex_numberstart,
-    .handleLToken = lex_numberhandle
-};
-
-static LexRule lr_string = {
-    .startsWith = lex_stringstart,
-    .handleLToken = lex_stringhandle
-};
-
+extern LexRule lr_whitespace;
+extern LexRule lr_numbers;
+extern LexRule lr_string;
 
 #endif //LEX_H
 
@@ -94,21 +103,44 @@ static LexRule lr_string = {
 #include <ctype.h>
 #include <stdio.h>
 
-static int lex_init(Lexer* lex, char* buff, uint32_t len){
+#ifdef lexdebug
+    #define dprintf(...) printf(__VA_ARGS__)
+#else
+    #define dprintf(...)
+#endif
+
+LexRule lr_whitespace = {
+    .startsWith = lex_whitespacestart,
+    .handleLToken = lex_whitespacehandle
+};
+
+LexRule lr_numbers = {
+    .startsWith = lex_numberstart,
+    .handleLToken = lex_numberhandle
+};
+
+LexRule lr_string = {
+    .startsWith = lex_stringstart,
+    .handleLToken = lex_stringhandle
+};
+
+LEKHADEF int lex_init(Lexer* lex, char* buff, uint32_t len){
     lex->start = buff;
     lex->len = len;
     lex->tokens.capacity=8;
     lex->tokens.list=(LToken*)calloc(8,sizeof(LToken));
     return LE_OK;
 }
-int lex_analyse(Lexer* lex, LexRules rules){
+LEKHADEF int lex_analyse(Lexer* lex, LexRules rules){
     while (lex->len > 0){
-	printf("remaining: %.*s\n",lex->len,lex->start);
+	dprintf("remaining: %.*s\n",lex->len,lex->start);
 	uint32_t blen = lex->len;
 	for(uint32_t i =0;i<rules.count;i++){
 	    LexRule rule=rules.list[i];
 	    if (rule.startsWith(lex->start) == LE_OK){
 		LexError status = (LexError)rule.handleLToken(lex, i); 
+		if (status == LE_OK)
+		    break;
 	    }
 	}
 	if (lex->len == blen){
@@ -118,23 +150,24 @@ int lex_analyse(Lexer* lex, LexRules rules){
     return LE_OK;
 }
 
-int lex_appendLToken(LTokens* toks, LToken tok){
+LEKHADEF int lex_appendLToken(LTokens* toks, LToken tok){
     if (toks->count == toks->capacity){
 	toks->list = (LToken*)realloc(toks->list, toks->capacity*2* sizeof(tok));
 	if (toks->list ==NULL){
 	    return LE_TOK_LIST_ERROR;
 	}
+	toks->capacity *= 2;
     }
     toks->list[toks->count]=tok;
     toks->count++;
     return LE_OK;
 }
 
-static int lex_stringstart(const char* buf){
+LEKHADEF int lex_stringstart(const char* buf){
     int res =(buf[0] - '"'); 
     return (res == LE_OK)? res : LE_MISMATCH_CHAR;
 }
-static int lex_stringhandle(Lexer* lex, uint32_t i){
+LEKHADEF int lex_stringhandle(Lexer* lex, uint32_t i){
     lex->len--;
     lex->start++;
     char* lexme = lex->start;
@@ -155,13 +188,13 @@ static int lex_stringhandle(Lexer* lex, uint32_t i){
     return lex_appendLToken(&(lex->tokens), t);
 }
 
-static int lex_numberstart(const char* buf){
+LEKHADEF int lex_numberstart(const char* buf){
     int isneg = buf[0] == '-';
     int ispos = isdigit(buf[0]);
     int res =isneg || ispos; 
     return (res == 1)? LE_OK : LE_MISMATCH_CHAR;
 }
-static int lex_numberhandle(Lexer* lex, uint32_t i){
+LEKHADEF int lex_numberhandle(Lexer* lex, uint32_t i){
     int len=0;
     int dec_count=0;
     char* lexme=lex->start;
@@ -192,19 +225,20 @@ static int lex_numberhandle(Lexer* lex, uint32_t i){
     return lex_appendLToken(&(lex->tokens), t);
 }
 
-static int lex_whitespacestart(const char* buf){
-    int res = isblank(buf[0]);
-    return (res == 1)? LE_OK : LE_MISMATCH_CHAR;
+LEKHADEF int lex_whitespacestart(const char* buf){
+    int res = isspace(buf[0]);
+    return (res != 0)? LE_OK : LE_MISMATCH_CHAR;
 }
-static int lex_whitespacehandle(Lexer* lex, uint32_t i){
+LEKHADEF int lex_whitespacehandle(Lexer* lex, uint32_t i){
     while(!lex_whitespacestart(lex->start) && lex->len > 0){
 	lex->len--;
 	lex->start++;
     }
+    (void)i;
     return LE_OK;
 }
 
-static void debugLTokens(LTokens* toks){
+LEKHADEF void debugLTokens(LTokens* toks){
     for (uint32_t i=0;i<toks->count;i++){
 	LToken t = toks->list[i];
 	printf("LToken ( lexme: %.*s , len:%d, type:%d)\n", t.size, t.lexme, t.size, t.type);
